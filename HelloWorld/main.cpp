5fb02ec6f21f4c64644b87a102e3d0c6240adf65
alexander <naitwa.alexandre@gmail.com>
2025-02-24 22:52:05 -0300

    first impressions

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,12 +1,6 @@
 #include <iostream>
 
 int main() {
-    const double PI = 3.14;
-    const int area = 5;
-
-    const double circle = PI * area / 2;
-
-    std::cout << circle << std::endl;
 
     return 0;
 }
