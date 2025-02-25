6ec4bba2bcd6f1db3bb8d6c4a12871e1f52526f0
alexander <naitwa.alexandre@gmail.com>
2025-02-25 20:35:10 -0300

    feat: console input intro

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    double x = 10;
-    double z = 5;
-    double y = (x + 10) / (3 * z);
-
-    std::cout << y;
-
+    const int sales = 95000;
+    const int stateTax = sales * 4 / 100;
+    const int countyTax = sales * 2 / 100;
+    const int totalTax = stateTax + countyTax;
+    cout << "sales = $" << sales << endl << "state tax = $" << stateTax << endl
+              << "county tax = $" << countyTax << endl << "total tax = $" << totalTax << endl;
     return 0;
 }
