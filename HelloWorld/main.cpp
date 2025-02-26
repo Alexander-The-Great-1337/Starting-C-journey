29e73abde563d1f3e0348ad704b933315ebf42b4
alexander <naitwa.alexandre@gmail.com>
2025-02-26 17:06:18 -0300

    fahrenheit to celsius

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <iomanip>
+
+using namespace std;
 
 int main() {
-    double x = 10;
-    double z = 5;
-    double y = (x + 10) / (3 * z);
+    cout << "Fahrenheit to Celsius" << endl;
 
-    std::cout << y;
+    cout << "Digit a value in Fahrenheit: ";
+    double fahrenheit;
+    cin >> fahrenheit;
+    double celsius = (fahrenheit - 32) / 1.8;
+
+    cout << "Celsius: " << fixed << setprecision(2) << celsius << endl;
 
     return 0;
 }
