eb2569270ecb968999c5cfedbab150dae8063b0a
alexander <naitwa.alexandre@gmail.com>
2025-03-03 09:19:33 -0300

    math built-in functions

---
+++ BroCodeFullCourse/day01/math_related_functions.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+
+    std::cout << "Hello Math" << "\n";
+
+    double x = 3.14;
+    double y = 4;
+    double z;
+
+    //z = std::min(y, x);
+    //z = std::max(y, x);
+
+
+    //z = pow(2, 4);
+    //z = sqrt(9);
+    //z = abs(-3);
+    //z = round(x);
+    //z = ceil(x);
+    //z = floor(x);
+
+    std::cout << z << "\n";
+
+    return 0;
+}
