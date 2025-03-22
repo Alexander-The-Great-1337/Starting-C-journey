2ce194109b5139d1f9599a1cfb2da96c441558b2
alexander <naitwa.alexandre@gmail.com>
2025-03-22 11:17:07 -0300

    feat: sort array

--- BroCodeFullCourse/day11/sort_an_array.cpp
+++ BroCodeFullCourse/day11/sort_an_array.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
+#include <algorithm>
 
-int main() {
-  
+using namespace std;
 
-  
+int main()
+{
+  string animes[] = {
+      "Naturo",
+      "One piece",
+      "Deathnote",
+      "Overlord",
+      "Dragon ball",
+  };
+
+  int numbers[] = {
+      1,
+      5,
+      42,
+      1337,
+      10,
+      2,
+      3,
+  };
+
+  sort(numbers, numbers + sizeof(numbers) / sizeof(numbers[0]), greater<int>());
+
+  sort(animes, animes + sizeof(animes) / sizeof(animes[0]), greater<string>());
+
+  cout << "Sorted Numbers:\n";
+  for (int n : numbers)
+  {
+    cout << n << '\n';
+  }
+
+  cout << "\nSorted Animes:\n";
+  for (const string &s : animes)
+  {
+    cout << s << '\n';
+  }
 
   return 0;
 }
