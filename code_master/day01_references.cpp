46951d4bfa651c517d1be825219682ec549e1540
alexander <naitwa.alexandre@gmail.com>
2025-04-06 19:49:22 -0300

    day02 references

--- code_master/day01_references.cpp
+++ code_master/day01_references.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
 
   /*Reference different from pointers can't not be changed
-    Reference are identified by & */
+    Reference are identified by & (ampersand) and poitners by * (asterisk) */
 
     int number1 = 10;
     int number2 = 20;
@@ -14,8 +14,16 @@ int main() {
 
     numberRef = number2;
 
-    cout << number1 << '\n'  << number2 << '\n'   << numberRef << '\n';
+    int* numberPtr = &number1;
 
-  cin.get();
+    *numberPtr = number2;
+
+    cout << number1 << '\n'  << number2 << '\n'   << numberRef << '\n' << *numberPtr << '\n';
+
+  /*
+    The output of this program is 20,20,20 because when we set a reference to number1
+    numberRef is now pointing to the space in memory of number1 so when we change numberRef
+    by attribuating number2 to it we know that number1 is now 20 because numberRef is pointing direct to it
+  */
   return 0;
 }
