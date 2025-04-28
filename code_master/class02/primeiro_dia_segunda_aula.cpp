cdfc2c5dcd526531ee7b0395bb856b1d2ad5bde4
alexander <naitwa.alexandre@gmail.com>
2025-04-28 18:57:39 -0300

    classe 02

--- code_master/class02/primeiro_dia_segunda_aula.cpp
+++ code_master/class02/primeiro_dia_segunda_aula.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <string>
-
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
 using namespace std;
+
+void SetCharacter(char ch, int x, int y);
 
 int main()
 {
